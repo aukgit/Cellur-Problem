8689f1a5d76245df2fdc0dc16769d61f9924f77f
Alim Ul Karim <auk.junk@live.com>
2014-12-15 02:45:20 +0600

    Phase 2

--- CellPhone/CellPhone/DesignPattern/Interfaces/IBehaviourRinging.cs
+++ CellPhone/CellPhone/DesignPattern/Interfaces/IBehaviourRinging.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CellPhone {
+﻿namespace CellPhone.DesignPattern.Interfaces {
     public interface IBehaviourRinging {
+        void Start();
+
+        void Stop();
     }
 }
---
+++ CellPhone/CellPhone/DesignPattern/Interfaces/IFlashingBehaviour.cs
@@ -0,0 +1,12 @@
+﻿namespace CellPhone.DesignPattern.Interfaces {
+    public interface IFlashingBehaviour {
+    
+        bool IsFlashing {
+            set;
+        }
+
+        void StartFlashing();
+
+        void StopFlashing();
+    }
+}
