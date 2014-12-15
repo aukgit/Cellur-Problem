fdd7c38c84f5591db1dddcdea2df9d24b9d8dfe1
Alim Ul Karim <auk.junk@live.com>
2014-12-16 02:18:59 +0600

    Network Related methods added

--- CellPhone/CellPhone/CellPhonePattern/BluePrints/GlobalNetwork.cs
+++ CellPhone/CellPhone/CellPhonePattern/BluePrints/GlobalNetwork.cs
@@ -3,5 +3,13 @@
 namespace CellPhone.CellPhonePattern.BluePrints {
     public class GlobalNetwork {
         public List<LocalNetwork> Networks { get; set; }
+
+        public bool ConnectToLocalNetwork(Phone phone) {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ConnectToLocalNetwork(Phone phone, LocalNetwork localNetwrok) {
+            throw new System.NotImplementedException();
+        }
     }
 }
